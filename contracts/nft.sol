742086e239d3a018143b66f17db5c98df926a160
Marcus Nunes <marcusnunes_11@hotmail.com>
2023-04-01 22:11:44 -0300

    contract minted

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -4,31 +4,34 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 
-contract nft is ERC721URIStorage, Ownable {
+contract nft is ERC721URIStorage {
+
     uint256 public nextTokenId = 1;
 
-    constructor() ERC721("nft", "N") {}
+    string private _name = 'Cristo Redentor';
+    string private _symbol = 'RJ';
 
-    function mint(address to, string memory tokenURI) public onlyOwner {
-        uint256 tokenId = nextTokenId;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        nextTokenId++;
+    constructor() ERC721('NFT', "col") {}
+
+    function mint(address to) public {
+      string memory tokenURI = 'QmZaXjPtopoWHD7maEcC7HTjHDXmjwNpRtjY18FpFVSoHP';
+      uint256 tokenId = nextTokenId;
+      _safeMint(to, tokenId);
+      _setTokenURI(tokenId, tokenURI);
+      nextTokenId++;
     }
 
-    function ownerOf(uint256 tokenId) public view virtual override returns (address) {
-        return ERC721.ownerOf(tokenId);
+    function getBalance(address owner) public view returns (uint256) {
+      return balanceOf(owner);
     }
     
     function ipfsHashToURI(string memory ipfsHash) public pure returns (string memory) {
-        return string(abi.encodePacked("https://ipfs.io/ipfs/", ipfsHash));
+      return string(abi.encodePacked("https://ipfs.io/ipfs/", ipfsHash));
     }
 
-    function deleteToken(uint256 tokenId) public onlyOwner {
-        _burn(tokenId);
+    function deleteToken(uint256 tokenId) public payable {
+    _burn(tokenId);
     }
 }
--- test/nftTest.js
+++ test/nftTest.js
@@ -1,16 +1,21 @@
 const { expect } = require("chai");
 
-describe("TinyVillage Test", function() {
+
+describe("NFT Test", function() {
   it("Should mint nft", async function() {
 
     const accounts = await ethers.getSigners()
+    const accountAddress = accounts[0].address
 
     const nft = await ethers.getContractFactory("nft")
-    const nftDeployed = await nft.deploy();
 
-    await nftDeployed.mint()
-    const balance = await nftDeployed.balanceOf(accounts[0].address,0)
+    const nftDeployed = await nft.deploy()
+    
+    await nftDeployed.mint(accountAddress, 'location')
+
+    const balance = await nftDeployed.balanceOf(accountAddress)
     console.log(balance)
-    // expect(3s).to.equal(Number(balance.toString()))
+    console.log(id)
+    expect(1).to.equal(Number(balance.toString()))
   })
 })
